91aa10ef7ae08a6b19d978da501f79e83daf698a
chris <chris_xiong_liu@163.com>
2017-05-21 22:35:07 +0800

    Spring 7.8.1 Customizing beans using a BeanPostProcessor

--- src/main/java/com/spring/framework/example/injection/Foo.java
+++ src/main/java/com/spring/framework/example/injection/Foo.java
@@ -1,12 +1,12 @@
 package com.spring.framework.example.injection;
 
 public class Foo {
-	private Baz baz;
-	private Bar bar;
+//	private Baz baz;
+//	private Bar bar;
 	
 	public Foo(Baz baz, Bar bar) {
 		super();
-		this.baz = baz;
-		this.bar = bar;
+//		this.baz = baz;
+//		this.bar = bar;
 	}
 }
---
+++ src/test/java/com/spring/fremework/example/Boot.java
@@ -0,0 +1,14 @@
+package com.spring.fremework.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.framework.example.injection.ComplexObject;
+
+public class Boot {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] {"com/spring/framework/example/applicationContext.xml"});
+		ComplexObject parent = (ComplexObject)ctx.getBean("parent");
+		System.out.println(parent);
+	}
+}
