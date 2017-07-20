b78a3ac1daff6288d8e46ab9f19f7c5f67cce9f8
Xiong.Liu <Xiong.Liu@CDPC00448.symbio.com>
2017-07-20 18:04:03 +0800

    9.5 Spring Type Conversion

---
+++ src/main/java/com/spring/framework/example/bean/Customer.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.spring.framework.example.bean;
+
+/**
+ * @author Xiong.Liu
+ *
+ */
+public class Customer {
+	private String firstName;
+	private String secondName;
+	private Address address;
+	/**
+	 * Gets firstName
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * Sets firstName the firstName
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * Gets secondName
+	 * @return the secondName
+	 */
+	public String getSecondName() {
+		return secondName;
+	}
+	/**
+	 * Sets secondName the secondName
+	 * @param secondName the secondName to set
+	 */
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	/**
+	 * Gets address
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	/**
+	 * Sets address the address
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
---
+++ src/main/java/com/spring/framework/example/validator/CustomerValidator.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.spring.framework.example.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.spring.framework.example.bean.Address;
+import com.spring.framework.example.bean.Customer;
+
+/**
+ * @author Xiong.Liu
+ *
+ */
+public class CustomerValidator implements Validator{
+
+	private final Validator addressValidator;
+	
+	@Autowired
+	public CustomerValidator(Validator addressValidator) {
+		if (addressValidator == null) {
+			throw new IllegalArgumentException("The supplied [Validator] is " +
+	                "required and must not be null.");
+		}
+		
+		if (!addressValidator.supports(Address.class)) {
+			throw new IllegalArgumentException("The supplied [Validator] must " +
+	                "support the validation of [Address] instances.");
+		}
+		this.addressValidator = addressValidator;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.springframework.validation.Validator#supports(java.lang.Class)
+	 */
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return Customer.class.isAssignableFrom(clazz);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.validation.Validator#validate(java.lang.Object, org.springframework.validation.Errors)
+	 */
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmpty(errors, "firstName", "field.required");
+		ValidationUtils.rejectIfEmpty(errors, "secondName", "field.required");
+		Customer customer = (Customer) target;
+		try {
+			errors.pushNestedPath("address");
+			ValidationUtils.invokeValidator(this.addressValidator, customer.getAddress(), errors);
+		} finally {
+			errors.popNestedPath();
+		}
+	}
+
+}
