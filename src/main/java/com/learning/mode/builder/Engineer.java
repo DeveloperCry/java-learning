c8fba138c3b022c9c691f8bcb6f28df2e523257e
Xiong.Liu <chris_xiong_liu@163.com>
2017-11-14 16:13:38 +0800

    Added builder

---
+++ src/main/java/com/learning/mode/builder/Engineer.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.learning.mode.builder;
+
+/**
+ * @author Xiong.Liu
+ *
+ */
+public class Engineer {
+	public void conduct(Builder builder) {
+		builder.buildFoundation();
+		builder.buildWall();
+		builder.buildRoof();
+		builder.buildDoor();
+		builder.buildWindow();
+	}
+}
