f273a24b7e46543dbb0fbac677c614e5761454ec
Xiong.Liu <Xiong.Liu@symbio.com>
2019-04-30 09:13:01 +0800

    string test

---
+++ src/main/java/com/learning/mode/extend/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.learning.mode.extend;
+
+/**
+ * @author Xiong.Liu
+ *
+ */
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Children();
+
+    }
+
+}
