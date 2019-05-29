4dfbf0fb610c1e4107142713a1408a6c897865a4
Xiong.Liu <Xiong.Liu@symbio.com>
2019-05-29 17:09:19 +0800

    Mediator

---
+++ src/main/java/com/learning/mode/mediator2/ConcreteColleague2.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.learning.mode.mediator2;
+
+/**
+ * @author Xiong.Liu
+ *
+ */
+public class ConcreteColleague2 extends AbstractColleague {
+
+    //通过构造函数传递中介者
+    public ConcreteColleague2(AbstractMediator _mediator){
+        super(_mediator);
+    }
+    //自有方法 self-method
+    public void selfMethod2(){
+        //处理自己的业务逻辑
+        System.out.println("This is concrete colleague2 self method1");
+    }
+    //依赖方法 dep-method
+    public void depMethod2(){
+        //处理自己的业务逻辑
+        //自己不能处理的业务逻辑，委托给中介者处理
+        super.mediator.doSomething2();
+    }
+
+}
