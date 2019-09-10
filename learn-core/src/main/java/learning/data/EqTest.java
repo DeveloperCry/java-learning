c7c4682134fa52d884f7b3487b1040a306bec626
chris <chris_xiong_liu@163.com>
2019-09-10 21:44:20 +0800

    add equals

--- learn-core/src/main/java/learning/data/EqTest.java
+++ learn-core/src/main/java/learning/data/EqTest.java
@@ -5,5 +5,12 @@ public class EqTest {
         Integer i = 7;
         Integer j = 7;
         System.out.println(i == j);
+
+        String str = "a" + "b" + "c" + "d" + "e";
+        System.out.println(str == "abcde");//true
+
+        String str2 = "a";
+        str2 = str2 + "c";
+        System.out.println(str2 == "ac");//false
     }
 }
