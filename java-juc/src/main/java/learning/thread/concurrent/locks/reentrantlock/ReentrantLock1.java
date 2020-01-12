f4bcb8aa9a34808452bd16979e47bd1e159ead1e
chris <chris_xiong_liu@163.com>
2020-01-12 22:43:41 +0800

    添加mq模块

---
+++ java-juc/src/main/java/learning/thread/concurrent/locks/reentrantlock/ReentrantLock1.java
@@ -0,0 +1,10 @@
+package learning.thread.concurrent.locks.reentrantlock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLock1 {
+    private final static ReentrantLock lock = new ReentrantLock();
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java-mq/src/main/java/learning/activemq/sender/Sender.java
@@ -0,0 +1,9 @@
+package learning.activemq.sender;
+
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
+
+public class Sender {
+    public static void main(String[] args) {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+    }
+}
