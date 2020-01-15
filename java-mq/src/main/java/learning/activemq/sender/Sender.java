8c75e350ed46dbdb99d56a3d84d10a496f6206af
chris <chris_xiong_liu@163.com>
2020-01-15 22:06:41 +0800

    添加mq sender

--- java-mq/src/main/java/learning/activemq/sender/Sender.java
+++ java-mq/src/main/java/learning/activemq/sender/Sender.java
@@ -1,9 +1,15 @@
 package learning.activemq.sender;
 
+import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.spring.ActiveMQConnectionFactory;
 
+import javax.jms.*;
+
 public class Sender {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JMSException {
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+        Connection connection = factory.createConnection();
+        Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue("mq");
     }
 }
