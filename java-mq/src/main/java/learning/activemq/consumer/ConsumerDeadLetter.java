630b1ee4f46c67daee956a2efa6627951c581589
Xiong.Liu <Xiong.Liu@symbio.com>
2020-02-04 13:45:40 +0800

    学习active MQ 死信队列

---
+++ java-mq/src/main/java/learning/activemq/consumer/ConsumerDeadLetter.java
@@ -0,0 +1,38 @@
+package learning.activemq.consumer;
+
+import learning.activemq.constants.Constants;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class ConsumerDeadLetter {
+    public static void main(String[] args) throws JMSException {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
+                ActiveMQConnectionFactory.DEFAULT_USER,
+                ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+                Constants.MQ_BROKER_URL);
+        Connection connection = factory.createConnection();
+        connection.start();
+        // 如果这里的事务(transacted)设置true,则需要手动提交消息确认(ACK)，否则不会发送消息确认(ACK)给MQ服务器，消息会被重复消费。
+        // 只有transacted设置为false，第二个参数才会生效，否则第二个参数会被Session.SESSION_TRANSACTED覆盖
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//        Queue destination = session.createQueue("mq_time_to_live");
+        Queue destination = session.createQueue("ActiveMQ.DLQ.Queue.dead_letter_mq");
+
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        consumer.setMessageListener(message -> {
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println(textMessage);
+                    System.out.println(textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("获取消息失败");
+            }
+        });
+    }
+}
---
+++ java-mq/src/main/java/learning/activemq/sender/SenderDeadLetter.java
@@ -0,0 +1,46 @@
+package learning.activemq.sender;
+
+import learning.activemq.constants.Constants;
+import learning.activemq.util.TimeUtil;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class SenderDeadLetter {
+    public static void main(String[] args) throws JMSException {
+        // 1. 建立工厂对象
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
+                ActiveMQConnectionFactory.DEFAULT_USER,
+                ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+                Constants.MQ_BROKER_URL);
+        // 2. 从工厂里拿一个连接
+        Connection connection = factory.createConnection();
+        connection.start();//这里一定不要忘记start，否则无法启动
+        // 3. 从连接中获取Session(会话)
+        // 如果这里的事务(transacted)设置true,则需要手动提交事务，否则消息不会被提交到MQ服务器。
+        // 并且只要消息没有被提交，则可以rollback；如果被提交了，则不能rollback
+        // 只有transacted设置为false，第二个参数才会生效，否则第二个参数会被Session.SESSION_TRANSACTED覆盖
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 4. 从会话中获取目的地(Destination)消费者会从这个目的地取消息
+        Queue destination = session.createQueue("dead_letter_mq");
+
+        // 5. 从会话中创建消息提供者
+        MessageProducer producer = session.createProducer(destination);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        producer.setTimeToLive(10 * 1000);
+
+        for (int i = 0; i < 10; i++) {
+            // 6. 从会话中创建文本消息(也可以创建其它类型的消息体)
+            TextMessage textMessage = session.createTextMessage("这是第" + i + "条activeMQ消息");
+            // 7. 通过消息提供者发送消息到ActiveMQ
+            producer.send(textMessage);
+            TimeUtil.timeSleep(2);
+        }
+
+
+        System.out.println("消息发送成功");
+
+        // 8. 关闭连接，这个一定不要忘记了
+        connection.close();
+    }
+}
