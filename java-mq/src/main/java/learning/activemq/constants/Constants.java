f4456e28ac88c40a79998d02f4a3e3deab5c7919
chris <chris_xiong_liu@163.com>
2020-02-04 19:18:47 +0800

    添加同步，消息延迟发送，消息堆积

--- java-mq/src/main/java/learning/activemq/constants/Constants.java
+++ java-mq/src/main/java/learning/activemq/constants/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public static final String MQ_PASSWORD = "admin";
     public static final String MQ_BROKER_URL = "tcp://localhost:61616";
 
+    public static final String MQ_SEC_USER = "user";
+    public static final String MQ_SEC_PASSWORD = "password1";
+
 }
