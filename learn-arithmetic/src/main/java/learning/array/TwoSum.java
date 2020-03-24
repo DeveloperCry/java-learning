abbcb52b48dd83780082c236cf37bb328f5d48fa
Xiong.Liu <Xiong.Liu@symbio.com>
2020-03-24 19:56:37 +0800

    修改算法

--- learn-arithmetic/src/main/java/learning/array/TwoSum.java
+++ learn-arithmetic/src/main/java/learning/array/TwoSum.java
@@ -4,14 +4,15 @@ public class TwoSum {
     public static void main(String[] args) {
 
         int[] nums = {2,7,11,15};
-        System.out.println(twoSum(nums, 9));
+        int[] result = twoSum(nums, 9);
+        System.out.println(result[0] + "," + result[1]);
     }
 
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
 
         for(int i = 0; i < nums.length - 1; i ++) {
-            for(int j = 1; j < nums.length; j ++) {
+            for(int j = i +1; j < nums.length; j ++) {
                 if(nums[i] + nums[j] == target) {
                     result[0] = i;
                     result[1] = j;
